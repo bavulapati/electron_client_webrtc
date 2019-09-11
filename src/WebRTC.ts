ae39eb682f92dfe4500b9acb954c57d46b4af34a
Balakrishna Avulapati <bavulapati@gmail.com>
2019-09-11 17:00:12 +0530

    change the turn server domain name

--- src/WebRTC.ts
+++ src/WebRTC.ts
@@ -179,7 +179,7 @@ function answerCall(room: string, socket: SocketIOClient.Socket): void {
         const servers: RTCConfiguration = {
             iceServers: [{
                 // urls: ['stun:stun.l.google.com:19302']
-                urls: ['turn:ec2-54-169-187-87.ap-southeast-1.compute.amazonaws.com:3478'],
+                urls: ['turn:bmrturn.idrivelite.com:3478'],
                 username: 'bmr-turn-user',
                 credential: 'insecure-key'
             }]
